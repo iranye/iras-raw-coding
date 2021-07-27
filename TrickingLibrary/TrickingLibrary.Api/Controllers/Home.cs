4fa8565d485df3c73d7eea59f2ec9b07d8453398
iranye <iranye@gmail.com>
2021-07-27 17:22:40 -0400

    Add API Project

---
+++ TrickingLibrary/TrickingLibrary.Api/Controllers/Home.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("[api/home]")]
+    public class Home : ControllerBase
+    {
+        private readonly ILogger<Home> _logger;
+
+        public Home(ILogger<Home> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
